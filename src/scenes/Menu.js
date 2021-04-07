30c57a40099279ffc93a85a44a1b0f8ba403e5ca
NikThomas9 <nikrthomas@gmail.com>
2021-04-07 14:50:20 -0700

    Scrolling Background

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,5 +1,10 @@
-class Meny extends Phaser.Scene {
+class Menu extends Phaser.Scene {
     constructor() {
-        
+        super("menuScene");
+    }
+
+    create() {
+        this.add.text(20,20, "Rocket Patrol Menu");
+        this.scene.start("playScene");
     }
 }
